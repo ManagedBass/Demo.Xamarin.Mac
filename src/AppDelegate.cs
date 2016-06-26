394bf9b04b43c1e6c6bbda0d9dca381dcd561adc
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-06-26 16:14:53 +0530

    Cleanup

---
+++ src/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using AppKit;
+using Foundation;
+using ManagedBass;
+
+namespace Xamarin.Mac.Player
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate
+	{
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			Bass.Init();
+		}
+
+		public override void WillTerminate(NSNotification notification)
+		{
+			Bass.Free();
+		}
+	}
+}
+
